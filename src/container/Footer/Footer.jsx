0f537adc49a154de0a92466bcad340536f3e7a46
sammyadewale1805 <fatokiadewale@gmail.com>
2026-03-18 16:31:34 -0700

    updated link

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                         <a href="https://github.com/sammyadewale1805" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
                         <a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
                         <a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
-                        <a href="www.linkedin.com/in/samuel-adewale-2060a71b6"target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a>
+                        <a href="https://www.linkedin.com/in/samuel-adewale-2060a71b6"target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a>
                     </div>
                 </div>
                 <div className="col-lg-6 col-md-6 col-12 text-center">
