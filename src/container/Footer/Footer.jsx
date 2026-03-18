366975c8eed4c4fba0bbc68ef49e1dd50f3ca4fa
sammyadewale1805 <fatokiadewale@gmail.com>
2026-03-18 16:16:07 -0700

    updated links

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,11 +8,12 @@ const Footer = () => {
             <div className="row align-items-center">
                 <div className="col-lg-6 col-md-6 col-12">
                     <div className="footer__links text-center">
-                        <a href="mailto:snone181@gmail.com"><i className="fas fa-envelope"></i></a>
-                        <a href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
-                        <a href="https://twitter.com/ssahibsingh_" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
-                        <a href="https://www.instagram.com/ssahibsingh_/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
-                        <a href="https://www.linkedin.com/in/ssahibsingh/" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a>
+                        <a href="#"><i className="fas fa-envelope"></i></a>
+                        <a href="https://github.com/sammyadewale1805" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a>
+                        <a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
+                        <a href="#" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
+                        <a href="www.linkedin.com/in/samuel-adewale-2060a71b6
+" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin-in"></i></a>
                     </div>
                 </div>
                 <div className="col-lg-6 col-md-6 col-12 text-center">
